ecf96228d5c146cae6fdf602f8dc5bab704f68e8
IMOitself <russjonn@gmail.com>
2025-06-21 10:12:44 +0800

    feat: continue install apk on button click

--- app/src/main/java/imo/after_build/ApkReceiverActivity.java
+++ app/src/main/java/imo/after_build/ApkReceiverActivity.java
@@ -3,7 +3,10 @@ package imo.after_build;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
@@ -36,6 +39,7 @@ public class ApkReceiverActivity extends Activity
         rootLayout.setOrientation(LinearLayout.VERTICAL);
         setTitle("sample.apk.package.name");
         projectFileListText.setText("app\n.gitignore\nbuild.gradle\nREADME.md");
+        addApkCheckbox.setChecked(true);
         addApkCheckbox.setText("Add Apk To Project");
         continueInstallBtn.setText("Continue Install Apk");
         setContentView(rootLayout);
@@ -46,6 +50,25 @@ public class ApkReceiverActivity extends Activity
             Toast.makeText(this, "You opened "+getClass()+" in the wrong way", Toast.LENGTH_LONG).show();
             finish();
         }
-        Uri apkUri = getIntent().getData();
+        
+        final Uri apkUri = getIntent().getData();
+        
+        continueInstallBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    if (!getPackageManager().canRequestPackageInstalls()) {
+                        startActivity(new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES, Uri.parse("package:" + getPackageName())));
+                        return;
+                    }
+                }
+                
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // Recommended for starting a new task
+                startActivity(intent);
+            }
+        });
     }
 }
